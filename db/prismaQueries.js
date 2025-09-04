8025566b604981c2fb138e791cba3f719663490f
Adrian-Koh <adriankoh216@yahoo.com>
2025-09-04 23:07:46 +0800

    Add query to clear high scores table

--- db/prismaQueries.js
+++ db/prismaQueries.js
@@ -1,8 +1,15 @@
 const prisma = require("./prismaClient");
+const { PrismaClient } = require("../generated/prisma");
 
 const printHighScores = async () => {
   const scores = await prisma.getTopTenHighScores();
   console.log("high scores: " + JSON.stringify(scores));
 };
 
-printHighScores();
+const clearHighScores = async () => {
+  const prismaClient = new PrismaClient();
+  const deletedScores = await prismaClient.highScores.deleteMany({});
+  console.log("deletedScores: " + JSON.stringify(deletedScores));
+};
+
+clearHighScores();
